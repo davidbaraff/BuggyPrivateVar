836ce739e968e4cf08536fe3fb519f2abf1ba8f8
David Baraff <davidbaraff@icloud.com>
2020-01-01 17:06:31 -0800

    fixes

--- Sources/BuggyPrivateVar/BuggyPrivateVar.swift
+++ Sources/BuggyPrivateVar/BuggyPrivateVar.swift
@@ -1,5 +1,13 @@
 public class JustWow<T> {
-    /*private*/ var notEvenUsed = [Int]()
+    
+    // If you comment out "private", then the regression test will not crash.
+    // As is, when you run the regression test on an iOS simulator, it crashes strangely
+    // (i.e. it messes up the stack, in the debugger).
+    //
+    // It works OK on macOS.
+    //
+    // The crash happens using Xcode 11.2.1 on both 10.14 and 10.15 systems.
+    private var notEvenUsed = [Int]()
 
     public func printHi() {
         print("Well, hi!")
